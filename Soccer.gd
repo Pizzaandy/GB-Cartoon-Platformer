c9f3119f4289b02d316987da086daf7da776b4e8
Revan Ji <jiruifancr@gmail.com>
2022-09-01 01:22:56 -0500

    add functional scoreboard

--- Soccer.gd
+++ Soccer.gd
@@ -3,13 +3,17 @@ extends Node2D
 var soccerball_res = preload("res://Scenes/SoccerBall.tscn")
 var confetti_res = preload("res://Confetti.tscn")
 
+var soccerball_inst
+
+var lscore = 0
+var rscore = 0
+
 onready var left_confetti_pos = $GoalLFront/GoalLConfettiAnchor
 onready var right_confetti_pos = $GoalRFront/GoalRConfettiAnchor
 
 func _ready():
-	self.add_child(soccerball_res.instance())
-	pass
-	
+	soccerball_inst = soccerball_res.instance()
+	self.add_child(soccerball_inst)
 
 
 func _on_GoalLArea_body_entered(body):
@@ -20,7 +24,10 @@ func _on_GoalLArea_body_entered(body):
 		confetti_inst.rotate_rad = 1
 		add_child(confetti_inst)
 		confetti_inst.global_position = pos
-	
+		soccerball_inst.queue_free()
+		soccerball_inst = soccerball_res.instance()
+		self.add_child(soccerball_inst)
+		lscore += 1
 
 
 func _on_GoalRArea_body_entered(body):
@@ -31,4 +38,11 @@ func _on_GoalRArea_body_entered(body):
 		confetti_inst.rotate_rad = -1
 		add_child(confetti_inst)
 		confetti_inst.global_position = pos
+		rscore += 1
+		soccerball_inst.queue_free()
+		soccerball_inst = soccerball_res.instance()
+		self.add_child(soccerball_inst)
+		
 	
+func _process(delta):
+	$Scores.text = str(lscore) + ":" + str(rscore)
